f649d025eeab6d1e31ea01675550cb1cd73da1ec
Michał Krawczyk <michal.krawczyk10@gmail.com>
2019-11-06 20:54:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 'use strict'
 
 function toggleMenu() {
-document.querySelector('.menu').classList.toggle('show')
+document.querySelector('.sidebar').classList.toggle('show')
+};
+function removeMenu() {
+	document.querySelector('.sidebar').classList.remove('show')
 };
 
 document.querySelector('.hamburger').addEventListener('click', function(e){
 	e.preventDefault();
 	toggleMenu();
+});
+document.querySelector('main').addEventListener('click', function(e){
+	e.preventDefault();
+	removeMenu();
 });
 
