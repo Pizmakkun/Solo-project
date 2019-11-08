99593c5d34be3dda9bcab720bff880003e58ec00
Michał Krawczyk <michal.krawczyk10@gmail.com>
2019-11-08 02:14:48 +0100

    2ndWeekDone

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 'use strict'
 
 function toggleMenu() {
-document.querySelector('.sidebar').classList.toggle('show')
+document.querySelector('.sidebar').classList.toggle('show');
+document.querySelector('.dim').classList.toggle('dimmed');
 };
+
 function removeMenu() {
-	document.querySelector('.sidebar').classList.remove('show')
+	document.querySelector('.sidebar').classList.remove('show');
+	document.querySelector('.dim').classList.remove('dimmed');
 };
 
 document.querySelector('.hamburger').addEventListener('click', function(e){
 	e.preventDefault();
 	toggleMenu();
 });
-document.querySelector('main').addEventListener('click', function(e){
-	e.preventDefault();
+document.querySelector('main').addEventListener('click', function(){
 	removeMenu();
 });
 
+function goBack() {
+	window.history.back();
+}
